8a2531d875339e1aeeed567283cf296292c1f38b
Joel <Joel.dun@pm.me>
2023-11-08 16:47:28 -0500

    Add constructor function in script.js

--- script.js
+++ script.js
@@ -14,3 +14,19 @@ Book.prototype.toggleReadStatus = function() {
 const myLibrary = [];
 const book1 = new Book("The Hobbit", "JRR Tolkien", 500, true)
 myLibrary.push(book1)
+
+
+// Function to display books
+function displayBooks() {
+  const bookList  = document.getElementById('book-list');
+  bookList.innerHTML = ''; //Clear existing content
+
+  myLibrary.forEach((book, index) => {
+    const bookInfo = document.createElement('div');
+    bookInfo.textContent = `Title: ${book.title}, Author: ${book.author}, Pages: ${book.pages}, Read: ${book.read ? 'Yes' : 'No'}`;
+    bookList.appendChild(bookInfo);
+  })
+}
+
+//Call the displayBooks to initially show the books
+displayBooks()
